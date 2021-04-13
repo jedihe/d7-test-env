88b1572103caa4becb0414f91c3e4b460c417ca2
John Herreño <jedihe@gmail.com>
2021-04-13 15:14:35 -0500

    first commit

---
+++ init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DRUPAL_BRANCH=7.x
+[ ! -d web ] && git clone --depth 1 --branch $DRUPAL_BRANCH https://git.drupalcode.org/project/drupal.git web
+
+lando start
+
+lando drush si --dburl="mysql://drupal7:drupal7@database/drupal7" --account-pass=admin -y
+
+echo "Running RdfCrudTestCase test with run-tests.sh..."
+echo "================================================"
+lando drush en -y simpletest
+lando run-tests --verbose --php /usr/local/bin/php --url "http://localhost" --class "RdfCrudTestCase"
+
+[[ -n $1 ]] &&
+  lando drush dl $1 -y
